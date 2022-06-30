27f315621d172e7c1e17c9203958cae8d9dbc4af
Israel Gonzalez <iisraelgonza@gmail.com>
2022-06-29 19:48:39 -0500

    1

--- app/inicio.php
+++ app/inicio.php
@@ -1,5 +1,5 @@
 <?php
-define("RUTA_APP","/works_iisraelg/Crud_Mvc_Paginado/");
+define("RUTA_APP","/works_iisraelg/GitHub/Crud_Mvc_Paginado/");
 require_once("libs/MySQLdb.php");
 require_once("libs/Controlador.php");
 require_once("libs/Control.php");
